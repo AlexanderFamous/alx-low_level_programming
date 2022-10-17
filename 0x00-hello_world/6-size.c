fad0e16c48593347f8cc0be8329390268093a559
Donchocho1 <ogagaoghene11@gmail.com>
2022-10-17 16:12:43 -0700

    correctio

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,14 @@ int main(void)
 {
 char charType;
 int intType;
+long int longType;
 long long int longLType;
 float floatType;
 
-printf("Size of char: %lu bytes\n",sizeof(charType));
-printf("Size of int: %lu bytes\n",sizeof(intType));
-printf("Size of long long int: %lu bytes\n",sizeof(longLType));
-printf("Size of float: %lu bytes\n",sizeof(floatType));
+printf("Size of char: %lu bytes\n", sizeof(charType));
+printf("Size of int: %lu bytes\n", sizeof(intType));
+printf("Size of long int: %lu bytes\n", sizeof(longType));
+printf("Size of long long int: %lu bytes\n", sizeof(longLType));
+printf("Size of float: %lu bytes\n", sizeof(floatType));
 return (0);
 }	
