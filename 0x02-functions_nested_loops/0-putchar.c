ed4d6cfd65609f9e2ee7c65d633de0f29f4a30ad
Donchocho1 <ogagaoghene11@gmail.com>
2022-10-20 09:45:57 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**main -entry point
+ *
+ * _putchar -writes the character to stdout
+ *
+ * Return: 0 on success
+ */
+
+int _putchar(char c);
+int main(void) 
+{
+
+      char c;
+     _putchar("%d_", c);
+     _putchar("%dp", c);
+     _putchar("%du", c);
+     _putchar("%dt", c);
+     _putchar("%dc", c);
+     _putchar("%dh", c);
+     _putchar("%da", c);
+     _putchar("%dr", c);
+     _putchar('\n');
+
+return (0);
+}
