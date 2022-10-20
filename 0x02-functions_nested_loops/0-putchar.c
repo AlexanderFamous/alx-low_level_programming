82a094310babc645f60fcf0339ba2a0a9f02966f
Donchocho1 <ogagaoghene11@gmail.com>
2022-10-20 12:38:36 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,19 @@
 #include "main.h"
-/**main -entry point
- *
- * _putchar -writes the character to stdout
+/*
+ * main - cheack the code
  *
  * Return: 0 on success
  */
-
-int _putchar(char c);
-int main(void) 
+int main(void)
 {
-
-      char c;
-     _putchar("_");
-     _putchar("p");
-     _putchar("u");
-     _putchar("t");
-     _putchar("c");
-     _putchar("h");
-     _putchar("a");
-     _putchar("r");
-     _putchar('\n');
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
