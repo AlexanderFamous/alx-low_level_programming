78438906dd686691dbd57c1ff1e12a01be5a801b
Donchocho1 <ogagaoghene11@gmail.com>
2022-10-28 17:58:33 -0700

    new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of two different digits, 
+ * main - Prints all possible combinations of two different digits,
  * in ascending order, separated by a comma followed by a space.
- * 
+ *
  * Return: Always 0.
  */
 
@@ -19,8 +19,7 @@ for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-
-if(digit1 == 8 && digit2 == 9)
+if (digit1 == 8 && digit2 == 9)
 continue;
 putchar(',');
 putchar(' ');
