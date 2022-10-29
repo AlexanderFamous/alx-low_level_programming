65a807b657bc37a64c39d271c59b9fd41abf0fa0
Donchocho1 <ogagaoghene11@gmail.com>
2022-10-28 18:47:09 -0700

    New Commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,30 +3,13 @@
 /**
  * _islower - a function that returns a value if its a lowercase
  *
- * Return: Always 0 (Success)
+ * Return : 1 if int c is lowercase, 0 if otherwise
  */
 
 int _islower(int c)
-int main(void)                                   
-{                                            
-int r;                                           
-r = _islower('H');                               
-_putchar(r + '0');                               
-r = _islower('o');                             
-_putchar(r + '0');                              
-r = _islower(108);                             
-_putchar(r + '0');                              
-_putchar('\n');                                 
-return (0);                                           
 {
-
-if(islower())
-{
-_putchar();
-}else
-{
-_putchar();
-}
+if (c >= 'a' && c <= 'z')
+return (1);
+else
 return (0);
-}
 }
