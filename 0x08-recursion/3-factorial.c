ae68411e99d1d768e923f7661d80fb68d8cdf983
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-02 20:09:29 -0700

    First attempt

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * int_factorial - finds the factorial of n numbers
+ * @n : an integer that stores the value of n
+ *
+ * Return: The fractorial of n number;
+ */
+
+int factorial(int n)
+{ 
+int result = n;
+if ( n < 0)
+
+
+return (-1);
+  
+
+else if( n >= 0 && n <= 1)
+
+return (1);
+
+result *= factorial (n - 1);
+
+return (result);
+}
+
