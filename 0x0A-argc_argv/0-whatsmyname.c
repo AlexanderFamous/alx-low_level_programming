717957f1d760760512e341f7b80e2cd330e4db25
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-09 15:25:12 -0800

    My First Commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,12 @@
  * @argc : holds the argument count
  * @argv : holds the strings of the argument bin the compiler
  *
- * Return : Always 0.
+ *Return: Always 0.
  */
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
 (void) argc;
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
