43089b2732bed52c8dec7a8eba8d67bdb990ee22
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-09 19:20:54 -0800

    Third Commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,14 +16,17 @@ for (num = 1 ; num < argc ; num++)
 for (digit = 0 ; argv[num][digit]; digit++)
 {
 
-if (argv[num][digit] < '0' || argv[num][digit] > '9')
+if (argv[num][digit] < '0') 
+{
+printf("0\n");
+}
+else if (argv[num][digit] > '9')
 {
 printf("Error\n");
 return (1);
 }
 }
 sum = sum + atoi(argv[num]);
-
 }
 printf("%d\n", sum);
 }
