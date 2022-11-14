34af9bf8b9eaabeaafe73c9046e459ed7f249b82
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-13 17:27:25 -0800

    Second commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,10 +12,16 @@
 char *str_concat(char *s1, char *s2)
 {
 
-int i = 0 , j = 0, len = 0;
+int i = 0, j = 0, len = 0;
 char *s3;
 
-s3 = malloc(sizeof(char)* (len + 1));
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+
+s3 = malloc(sizeof(char) * (len));
 /* insert the first string in the new string allocated dynamically */
 while (s1[i] != '\0')
 {
@@ -26,12 +32,12 @@ j++;
 i = 0;
 while (s2[i] != '\0')
 {
-s3[j] =s2[i];
+s3[j] = s2[i];
 i++;
 j++;
 }
 
-s3[j] = '\0';
+/* s3[j] = '\0'; */
 return (s3);
 
 
