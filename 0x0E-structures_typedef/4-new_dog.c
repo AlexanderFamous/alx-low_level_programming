e0d881f26f14f0eece64edd8cd8fa448052b8dc9
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-17 15:38:39 -0800

    Second Commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - A struct that givess some detail of a dog
+ * @name: First Member
+ * @age: Second Member
+ * @owner: Third Member
+ *
+ * Description: This struct datatype simply has two char member and
+ * an int to give details of a dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *d; 
+d = malloc(sizeof(dog_t)); 
+if (d == NULL)
+return (NULL);
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+return d;
+}
+
