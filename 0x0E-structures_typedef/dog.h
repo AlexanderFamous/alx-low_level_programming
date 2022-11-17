6733a0c916ac649cb140c32aa7a954faa8975970
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-17 15:24:38 -0800

    Second Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,13 @@
  * Description: Long Description
  */
 
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
 
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
