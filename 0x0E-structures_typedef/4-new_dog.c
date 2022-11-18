11f69c9ded942cdd050463f9768a9cadacc5a865
Donchocho1 <ogagaoghene11@gmail.com>
2022-11-18 03:08:20 -0800

    Third commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+int nlen, olen, i;
+
 dog_t *d;
+nlen = olen = 0;
+/* Loop through the string  owner and name*/
+while (name[nlen++])
+	;
+while (owner[olen++])
+	;
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
 
-d->name = name;
+/*allocate memory for d->name dynamically */
+d->name = malloc(nlen *sizeof(d->name));
+if (d == NULL)
+return (NULL);
+/* loop through the name sting and save a copy in another name variable */
+for (i = 0; i < nlen; i++)
+	d->name[i] = name[i];
+
 d->age = age;
-d->owner = owner;
+
+d->owner = malloc(olen * sizeof(d->owner));
+if (d == NULL)
+return (NULL);
+for (i = 0 ; i < olen; i++)
+d->owner[i] = owner[i];
+
 return (d);
 }
 
