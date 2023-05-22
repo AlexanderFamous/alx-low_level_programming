ef9a21ed9787be08781e54f6db7830029c5d9a4c
Emmanuel <97983309+Donchocho1@users.noreply.github.com>
2023-05-22 18:18:24 +0100

    Latest commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- The entry point
+ *
+ * Return: always 0 (successful)
+ */
+int main(void)
+{
+/*write(2,"\'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);*/
+fprintf(stderr,"\'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n\n");
+return (1);
+}
